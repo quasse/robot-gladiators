03a1ed64d565817ee55efa283a3641c10df9ca0b
Sam Morgen <samorgen14@gmail.com>
2021-05-08 14:34:06 -0700

    Javascript testing

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,11 @@
-window.alert("This is an alert!Javascript is running!");
+var playerName = window.prompt("What is your robot's name?");
+
+console.log("Our robot's name is " + playerName);
+
+var name = "Sam";
+console.log(name);
+
+function fight() {
+  window.alert("The fight has begun!");
+}
+//fight();
